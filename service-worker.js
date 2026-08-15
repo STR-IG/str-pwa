fa62e6ca6fe5f561b864a13447b34c26cd104955
STR-IG <str.instituto.grifols@gmail.com>
2026-08-16 01:54:08 +0200

    Implement caching for navigate requests
    Add cache handling for navigate requests in service worker

--- service-worker.js
+++ service-worker.js
@@ -23,6 +23,23 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request, { cache: 'no-store' })
+        .then((response) => {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, copy);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
   event.respondWith(
     fetch(event.request)
       .then((response) => {
